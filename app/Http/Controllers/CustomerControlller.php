506b45c5212e89d66a5aae8a912e76043c1f64cc
lam.dinh <lam.dinh@asvnpnpc63.pn.com.vn>
2019-05-10 08:26:27 +0700

    Edited route and created controllers: Admin, Customer, Partner

---
+++ app/Http/Controllers/CustomerControlller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CustomerControlller extends Controller
+{
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,37 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | contains the "web" middleware group. Now create something great!
+  |
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('partner', function () {
+/*Route::get('partner', function () {
     return view('partner-management-dashboard.index-section');
 });
 
 Route::get('partner2', function () {
     return view('partner-management-dashboard.product-section');
+});*/
+
+
+Route::prefix('partner')->group(function() {
+    
+});
+
+Route::prefix('admin')->group(function() {
+    
+});
+
+Route::prefix('customer')->group(function() {
+    
 });
